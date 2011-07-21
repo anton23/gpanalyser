f6acb1656a3c61bb7343a02e02683972ce890618
tonkos <tonkos@localhost>
2011-07-22 00:38:41 +0200

    Added compare analysis postprocessor.

--- src-pctmc/uk/ac/imperial/doc/pctmc/compare/PCTMCCompareAnalysis.java
+++ src-pctmc/uk/ac/imperial/doc/pctmc/compare/PCTMCCompareAnalysis.java
@@ -6,10 +6,9 @@ import uk.ac.imperial.doc.jexpressions.constants.Constants;
 import uk.ac.imperial.doc.pctmc.analysis.AbstractPCTMCAnalysis;
 import uk.ac.imperial.doc.pctmc.expressions.CombinedPopulationProduct;
 
-//TODO need a postprocessor
+
 public class PCTMCCompareAnalysis extends AbstractPCTMCAnalysis{
-
-
+	
 	@Override
 	public void prepare(Constants constants) {
 		analysis1.prepare(constants);
@@ -22,10 +21,19 @@ public class PCTMCCompareAnalysis extends AbstractPCTMCAnalysis{
 	}
 
 	private AbstractPCTMCAnalysis analysis1, analysis2; 
+	
+	
+
+	public AbstractPCTMCAnalysis getAnalysis1() {
+		return analysis1;
+	}
+
+	public AbstractPCTMCAnalysis getAnalysis2() {
+		return analysis2;
+	}
 
 	public PCTMCCompareAnalysis(AbstractPCTMCAnalysis analysis1, AbstractPCTMCAnalysis analysis2) {
 		super(analysis1.getPCTMC());
-		
 		this.analysis1 = analysis1; 
 		this.analysis2 = analysis2;
 	} 
@@ -35,40 +43,5 @@ public class PCTMCCompareAnalysis extends AbstractPCTMCAnalysis{
 			Collection<CombinedPopulationProduct> combinedProducts) {
 			analysis1.setUsedMoments(combinedProducts);
 			analysis2.setUsedMoments(combinedProducts);
-	}
-	
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result
-				+ ((analysis1 == null) ? 0 : analysis1.hashCode());
-		result = prime * result
-				+ ((analysis2 == null) ? 0 : analysis2.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PCTMCCompareAnalysis other = (PCTMCCompareAnalysis) obj;
-		if (analysis1 == null) {
-			if (other.analysis1 != null)
-				return false;
-		} else if (!analysis1.equals(other.analysis1))
-			return false;
-		if (analysis2 == null) {
-			if (other.analysis2 != null)
-				return false;
-		} else if (!analysis2.equals(other.analysis2))
-			return false;
-		return true;
 	}
 }
