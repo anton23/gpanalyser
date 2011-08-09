144c70992310f1c55a749b26a469bb834d69399c
tonkos <tonkos@gmail.com>
2011-08-09 23:12:26 +0200

    Numerical postprocessor also stores computed values of the assigned plot descriptions.

--- src-examples/ExampleUseOfAPI.java
+++ src-examples/ExampleUseOfAPI.java
@@ -1,6 +1,12 @@
+import java.util.Map;
+
+import org.jfree.data.xy.XYDataset;
+
 import uk.ac.imperial.doc.gpa.GPAPMain;
 import uk.ac.imperial.doc.pctmc.analysis.AbstractPCTMCAnalysis;
+import uk.ac.imperial.doc.pctmc.analysis.AnalysisUtils;
 import uk.ac.imperial.doc.pctmc.analysis.plotexpressions.PlotDescription;
+import uk.ac.imperial.doc.pctmc.charts.PCTMCChartUtilities;
 import uk.ac.imperial.doc.pctmc.interpreter.PCTMCFileRepresentation;
 import uk.ac.imperial.doc.pctmc.interpreter.PCTMCInterpreter;
 import uk.ac.imperial.doc.pctmc.interpreter.ParseException;
@@ -19,11 +25,23 @@ public class ExampleUseOfAPI {
 			
 			AbstractPCTMCAnalysis analysis = fileRepresentation.getPlots().keySet().iterator().next();
 			System.out.println("The first analysis has these postprocessors:\n"+analysis.getPostprocessors());
-			((NumericalPostprocessor)analysis.getPostprocessors().iterator().next()).setStopTime(50.0);
+			
+			NumericalPostprocessor numericalPostprocessor = (NumericalPostprocessor)analysis.getPostprocessors().iterator().next();
+			numericalPostprocessor.setStopTime(50.0);
 			
 			fileRepresentation.getPlots().put(analysis,new PlotDescription(gpepaInterpreter.parseExpressionList("Var[Clients:Client]^0.5, Var[Servers:Server]^0.5")));
 			
 			gpepaInterpreter.processFileRepresentation(fileRepresentation);
+			
+			Map<PlotDescription, double[][]> results = numericalPostprocessor.getResults();
+						
+			for (Map.Entry<PlotDescription, double[][]> e:results.entrySet()){
+				double stepSize = numericalPostprocessor.getStepSize();
+				XYDataset dataset = AnalysisUtils.getDataset(e.getValue(), stepSize, e.getKey().getLabels());
+				PCTMCChartUtilities.nextBatch();
+				PCTMCChartUtilities.drawChart(dataset, "time", "count", "",
+						"Example plots from postprocessors");
+			}
 			
 		} catch (ParseException e) {
 			e.printStackTrace();
