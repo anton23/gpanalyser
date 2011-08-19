f1b0ccd88f621cb6c7485754c1c87ead630dbf76
tonkos <tonkos@gmail.com>
2011-08-20 01:29:14 +0200

    PCTMCIterate stores results.

--- src-examples/ExampleUseOfAPI.java
+++ src-examples/ExampleUseOfAPI.java
@@ -2,13 +2,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import uk.ac.imperial.doc.gpa.GPAPMain;
 import uk.ac.imperial.doc.pctmc.analysis.AbstractPCTMCAnalysis;
 import uk.ac.imperial.doc.pctmc.analysis.AnalysisUtils;
 import uk.ac.imperial.doc.pctmc.analysis.plotexpressions.PlotDescription;
+import uk.ac.imperial.doc.pctmc.charts.ChartUtils3D;
 import uk.ac.imperial.doc.pctmc.charts.PCTMCChartUtilities;
 import uk.ac.imperial.doc.pctmc.experiments.iterate.PCTMCIterate;
+import uk.ac.imperial.doc.pctmc.experiments.iterate.PlotAtDescription;
 import uk.ac.imperial.doc.pctmc.experiments.iterate.RangeSpecification;
 import uk.ac.imperial.doc.pctmc.interpreter.PCTMCFileRepresentation;
 import uk.ac.imperial.doc.pctmc.interpreter.PCTMCInterpreter;
@@ -48,17 +51,40 @@ public class ExampleUseOfAPI {
 						"Example plots from postprocessors");
 			}
 			
-			PCTMCIterate experiment = fileRepresentation.getExperiments().iterator().next();
-			System.out.println("The first iterate experiment is " + experiment);
-			List<RangeSpecification> ranges = experiment.getRanges();
-			for (RangeSpecification r:ranges){
-				System.out.println("Constant: " + r.getConstant());
-				System.out.println("From: " + r.getFrom());
-				System.out.println("To: " + r.getTo());
-				System.out.println("Steps: " + r.getSteps());
-				System.out.println("Dc: " + r.getDc());
+			for (PCTMCIterate experiment:fileRepresentation.getExperiments()){
+				int dim = experiment.getRanges().size();
+			    System.out.println("\nExperiment " + experiment);
+			    System.out.println("The sweeping dimension is " + dim);
+				List<RangeSpecification> ranges = experiment.getRanges();
+				for (RangeSpecification r:ranges){
+					System.out.println("Constant: " + r.getConstant());
+					System.out.println("From: " + r.getFrom());
+					System.out.println("To: " + r.getTo());
+					System.out.println("Steps: " + r.getSteps());
+					System.out.println("Dc: " + r.getDc());
+				}
+				
+				if (dim==1){
+					System.out.println("The results are standard plots:");
+					for (Map.Entry<PlotAtDescription, double[][]> e:experiment.getResults().entrySet()){
+						RangeSpecification range = experiment.getRanges().get(0);
+						XYSeriesCollection dataset = AnalysisUtils.getDataset(e.getValue(), 
+								range.getFrom(),range.getDc(), 
+			    		  new String[]{e.getKey().toString()});
+			    		PCTMCChartUtilities.drawChart(dataset, range.getConstant(), "count", "", "Example " + experiment.toShortString());
+					}
+				}
+				if (dim==2){
+					for (Map.Entry<PlotAtDescription, double[][]> e:experiment.getResults().entrySet()){
+						RangeSpecification xRange = experiment.getRanges().get(0);
+						RangeSpecification yRange = experiment.getRanges().get(1);
+						ChartUtils3D.drawChart("Example "+experiment.toShortString(),
+								e.getKey().toString(),e.getValue(),xRange.getFrom(),xRange.getDc(),yRange.getFrom(),yRange.getDc(),
+					    		xRange.getConstant(),yRange.getConstant(),e.getKey().getExpression().toString());
+					}
+				}
+				
 			}
-
 			
 			
 		} catch (ParseException e) {
