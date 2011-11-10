6853e688ba1bf73ffdf3b2593b7cc77162eae432
tonkos <tonkos@localhost>
2011-11-10 18:07:01 +0000

    Added error reporting methods to plain parser.

--- src-pctmc/uk/ac/imperial/doc/pctmc/syntax/GPAParsingData.java
+++ src-pctmc/uk/ac/imperial/doc/pctmc/syntax/GPAParsingData.java
@@ -3,14 +3,22 @@ package uk.ac.imperial.doc.pctmc.syntax;
 import java.util.Set;
 
 public class GPAParsingData extends ParsingData {
-	 protected Set<String> componentNames;
+	protected Set<String> componentNames;
+	protected Set<String> groupLabels;
 
-	public GPAParsingData(Set<String> componentNames) {
+	public GPAParsingData(Set<String> componentNames, Set<String> groupLabels) {
 		super();
 		this.componentNames = componentNames;
+		this.groupLabels = groupLabels;
 	}
 
 	public Set<String> getComponentNames() {
 		return componentNames;
 	}
+
+	public Set<String> getGroupNames() {
+		return groupLabels;
+	}
+	
+	
 }
