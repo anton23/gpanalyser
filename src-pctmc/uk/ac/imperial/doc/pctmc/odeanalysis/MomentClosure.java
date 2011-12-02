93adcf0c6145fe53e8947fb67b864dfb6a1c6a4e
tonkos <tonkos@localhost>
2011-12-02 17:43:13 +0000

    Tiny change.

--- src-pctmc/uk/ac/imperial/doc/pctmc/odeanalysis/MomentClosure.java
+++ src-pctmc/uk/ac/imperial/doc/pctmc/odeanalysis/MomentClosure.java
@@ -7,10 +7,9 @@ import uk.ac.imperial.doc.pctmc.expressions.CombinedPopulationProduct;
 import uk.ac.imperial.doc.pctmc.expressions.PopulationProduct;
 
 public abstract class MomentClosure {
-	
 	public MomentClosure(Map<String, Object> parameters) {}
 	
-	public abstract AbstractExpression closeRate(AbstractExpression rate, PopulationProduct moment);
+	public abstract AbstractExpression insertProductIntoRate(AbstractExpression rate, PopulationProduct moment);
 	public abstract AbstractExpression insertAccumulations(AbstractExpression derivative, CombinedPopulationProduct moment);
 	public abstract String toString();
 
