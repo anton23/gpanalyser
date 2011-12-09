85f4970f06b1619722708f369ffb4e02160122cb
machr666 <machr666@localhost>
2011-12-09 15:08:24 +0000

    Bug fixes

--- src-masspa/uk/ac/imperial/doc/masspa/expressions/ExpressionEvaluatorWithLocationConstants.java
+++ src-masspa/uk/ac/imperial/doc/masspa/expressions/ExpressionEvaluatorWithLocationConstants.java
@@ -4,6 +4,7 @@ import uk.ac.imperial.doc.jexpressions.constants.ConstantExpression;
 import uk.ac.imperial.doc.jexpressions.constants.Constants;
 import uk.ac.imperial.doc.jexpressions.constants.visitors.ExpressionEvaluatorWithConstants;
 import uk.ac.imperial.doc.masspa.representation.model.Location;
+import uk.ac.imperial.doc.masspa.representation.model.VarLocation;
 
 public class ExpressionEvaluatorWithLocationConstants extends ExpressionEvaluatorWithConstants
 {
@@ -18,7 +19,7 @@ public class ExpressionEvaluatorWithLocationConstants extends ExpressionEvaluato
 	public void visit(ConstantExpression e)
 	{
 		// Try to find better match constName@location instead of const
-		Double parameterValue = constants.getConstantValue(e.getConstant().toString().replace("@(_)", "")+loc.toString());
+		Double parameterValue = constants.getConstantValue(e.getConstant().toString().replace(VarLocation.getInstance().toString(), "")+loc.toString());
 		if (parameterValue == null){
 			super.visit(e);
 			return;
