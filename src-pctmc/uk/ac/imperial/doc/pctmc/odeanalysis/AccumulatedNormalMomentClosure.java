18d2b4c14547d719a20b4086c0d73a2efdfffa69
tonkos <tonkos@localhost>
2011-12-13 19:26:20 +0000

    Added an experimental moment closure for rates involving accumulations.

---
+++ src-pctmc/uk/ac/imperial/doc/pctmc/odeanalysis/AccumulatedNormalMomentClosure.java
@@ -0,0 +1,40 @@
+package uk.ac.imperial.doc.pctmc.odeanalysis;
+
+import java.util.Map;
+
+import uk.ac.imperial.doc.jexpressions.expressions.AbstractExpression;
+import uk.ac.imperial.doc.pctmc.expressions.CombinedPopulationProduct;
+import uk.ac.imperial.doc.pctmc.expressions.PopulationProduct;
+
+public class AccumulatedNormalMomentClosure extends NormalMomentClosure {
+
+	public final static String NAME = "AccumulatedNormalClosure";
+	
+	public AccumulatedNormalMomentClosure(int maxOrder) {
+		super(maxOrder);		
+	}
+	
+	public AccumulatedNormalMomentClosure(Map<String, Object> parameters) {
+		super(parameters);
+	}
+
+	@Override
+	public AbstractExpression insertProductIntoRate(AbstractExpression rate,
+			PopulationProduct moment) {
+		AccumulatedNormalClosureVisitor visitor = new AccumulatedNormalClosureVisitor(moment, maxOrder);
+		rate.accept(visitor);
+		return visitor.getResult();
+	}
+	
+	@Override
+	public AbstractExpression insertAccumulations(
+			AbstractExpression derivative,
+			CombinedPopulationProduct moment) {
+		IntegralInsterterVisitor visitor = new AccumulatedIntegralInsterterVisitor(
+				new CombinedPopulationProduct(null, moment
+						.getAccumulatedProducts()));
+		derivative.accept(visitor);
+		return visitor.getResult();
+	}
+
+}
