8d2e33526b68a194ea8b7a2a030b0245fb0f29ca
DeadLine <DeadLine@DeadLine-VAIO.inuknetworks.com>
2012-02-19 00:42:51 +0000

    - wrote Global Passage Time point-mass approximation function

---
+++ src-gpa/uk/ac/imperial/doc/gpa/probes/GlobalProbeExpressions/BothUExpression.java
@@ -0,0 +1,48 @@
+package uk.ac.imperial.doc.gpa.probes.GlobalProbeExpressions;
+
+public class BothUExpression extends AbstractUExpression
+{
+    private AbstractUExpression R1;
+    private AbstractUExpression R2;
+
+    public BothUExpression (AbstractUExpression r1, AbstractUExpression r2)
+    {
+        R1 = r1;
+        R2 = r2;
+    }
+
+    public AbstractUExpression getR1 ()
+    {
+        return R1;
+    }
+
+    public AbstractUExpression getR2 ()
+    {
+        return R2;
+    }
+
+    @Override
+    public boolean equals (Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass () != o.getClass ()) return false;
+
+        BothUExpression that = (BothUExpression) o;
+
+        return R1.equals (that.R1) && R2.equals (that.R2);
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        int result = R1.hashCode ();
+        result = 3 + 31 * result + R2.hashCode ();
+        return result;
+    }
+
+    @Override
+    public void accept (UExpressionVisitor v, double time)
+    {
+        v.visit (this, time);
+    }
+}
---
+++ src-gpa/uk/ac/imperial/doc/gpa/probes/GlobalProbeExpressions/UPrimeExpression.java
@@ -0,0 +1,43 @@
+package uk.ac.imperial.doc.gpa.probes.GlobalProbeExpressions;
+
+import uk.ac.imperial.doc.gpepa.states.GPEPAActionCount;
+
+import java.util.Set;
+
+public class UPrimeExpression extends AbstractUExpression
+{
+    Set<GPEPAActionCount> actions;
+
+    public UPrimeExpression (Set<GPEPAActionCount> actions)
+    {
+        this.actions = actions;
+    }
+
+    public Set<GPEPAActionCount> getActions ()
+    {
+        return actions;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass () != o.getClass ()) return false;
+
+        UPrimeExpression that = (UPrimeExpression) o;
+
+        return actions.equals (that.actions);
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        return actions.hashCode ();
+    }
+
+    @Override
+    public void accept (UExpressionVisitor v, double time)
+    {
+        v.visit (this, time);
+    }
+}
