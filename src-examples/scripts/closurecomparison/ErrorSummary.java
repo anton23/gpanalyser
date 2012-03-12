989df8cf14992cc618f8e057cd58615a5d43f37f
tonkos <tonkos@gmail.com>
2012-03-12 02:53:25 +0000

    Continued with multithreaded closure comparison.

---
+++ src-examples/scripts/closurecomparison/ErrorSummary.java
@@ -0,0 +1,39 @@
+package scripts.closurecomparison;
+
+public class ErrorSummary {
+	protected double relativeAccumulated;
+	protected double maxRelative;
+	protected double averageRelative;
+	
+	public ErrorSummary(double relativeAccumulated, double maxRelative,
+			double averageRelative) {
+		super();
+		this.relativeAccumulated = relativeAccumulated;
+		this.maxRelative = maxRelative;
+		this.averageRelative = averageRelative;
+	}
+
+	public double getRelativeAccumulated() {
+		return relativeAccumulated;
+	}
+
+	public double getMaxRelative() {
+		return maxRelative;
+	}
+
+	public double getAverageRelative() {
+		return averageRelative;
+	}
+
+	public void setRelativeAccumulated(double relativeAccumulated) {
+		this.relativeAccumulated = relativeAccumulated;
+	}
+
+	public void setMaxRelative(double maxRelative) {
+		this.maxRelative = maxRelative;
+	}
+
+	public void setAverageRelative(double averageRelative) {
+		this.averageRelative = averageRelative;
+	}
+}
