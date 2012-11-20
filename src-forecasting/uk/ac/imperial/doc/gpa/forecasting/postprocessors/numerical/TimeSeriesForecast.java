9df2bf1f324960df4905309cd4c06eaf824061f9
machr666 <machr666@localhost>
2012-11-20 11:45:37 +0000

    Added plotting to this class

--- src-forecasting/uk/ac/imperial/doc/gpa/forecasting/postprocessors/numerical/TimeSeriesForecast.java
+++ src-forecasting/uk/ac/imperial/doc/gpa/forecasting/postprocessors/numerical/TimeSeriesForecast.java
@@ -1,14 +1,18 @@
 package uk.ac.imperial.doc.gpa.forecasting.postprocessors.numerical;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.jfree.data.xy.XYDataset;
+
 import uk.ac.imperial.doc.gpa.forecasting.util.FileExtra;
 import uk.ac.imperial.doc.gpa.plain.representation.PlainPCTMC;
 import uk.ac.imperial.doc.gpa.plain.representation.timed.TimedEvents;
+import uk.ac.imperial.doc.pctmc.analysis.AnalysisUtils;
+import uk.ac.imperial.doc.pctmc.charts.PCTMCChartUtilities;
 import uk.ac.imperial.doc.pctmc.representation.State;
 import uk.ac.imperial.doc.pctmc.utils.PCTMCLogging;
 
@@ -30,6 +34,7 @@ public class TimeSeriesForecast {
 	private List<String> mDepTSFiles;
 	private double[][] mDepTS;
 	private int mTSStartIndex;
+	private String mCurDate;
 	
 	public TimeSeriesForecast(PlainPCTMC pctmc, int warmup, int forecast,
 			int ibf, State arrState, List<State> startStates,
@@ -79,6 +84,7 @@ public class TimeSeriesForecast {
 		
 		// Expecting 1 observation per time unit
 		int numStartStates = mStartStates.size();
+		mCurDate = mArrTSFiles.get(0).replaceAll("Arrivals", "").replaceAll(".dat","");	
 		String arrTSStr = FileExtra.readFromTextFile(mArrTSFiles.remove(0));
 		String depTSStr = FileExtra.readFromTextFile(mDepTSFiles.remove(0));
 
@@ -102,10 +108,10 @@ public class TimeSeriesForecast {
 		
 		// We start every time series from the first observation in the time series
 		mTSStartIndex = 0;
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Run the forecast for a subset of our data at a time. From one forecast
 	 * to another we leave an mIBF minute gap. The warmup period will be used
@@ -140,7 +146,7 @@ public class TimeSeriesForecast {
 			
 			// We assume mu rate changes to be inhomogeneous but deterministic
 			// Thus we can also anticipate changes in mu during the forecast
-			// period
+			// period using the average mu observed in the training data
 			for (int t=mWarmup; t<mWarmup+mForecast; ++t) {
 				int relativeTime = t*mTSStep;
 				destRateEvents[t][0] = relativeTime;
@@ -155,7 +161,7 @@ public class TimeSeriesForecast {
 			// To do that we find the gradient of change from the previous windowSize
 			// minute window to the current one and then extrapolate the rate of the
 			// current forecast window
-			int windowSize = 5;
+			int windowSize = 10;
 			double[] fcastRates = new double[windowSize+mForecast];
 			// Copy the last windowSize measurements into fcastRates
 			for (int t=0; t < windowSize; ++t)
@@ -202,5 +208,53 @@ public class TimeSeriesForecast {
 		mTSStartIndex += mIBF;
 		return actualArr;
 	}
-
+	
+	/**
+	 * @param mData (for each time point we expect {modelMeanArrivals,modelStdDevArrivals,actualNumArrivals}
+	 * @param stepSize granularity with which the data is collected
+	 * @param analysis name of analysis
+	 */
+	public void plotForecast(List<double[]> mData, double stepSize, String analysis)
+	{
+		int numDataPts = mData.size();
+		int lag = mForecast * ((int)(1/stepSize));
+		String[] names =  {"Empirical ([t,t+"+mForecast+"])","E[Forecast([t,t+"+mForecast+"]) ]"};
+		String[] namesLag =  {"Empirical ([t-"+mForecast+",t])","E[Forecast([t,t+"+mForecast+"]) ]"};
+		double[][] data = new double[numDataPts+lag][2];
+		double[][] dataStdDev = new double[numDataPts+lag][2];
+		double[][] dataLag = new double[numDataPts+lag][2];
+		double[][] dataLagStdDev = new double[numDataPts+lag][2];
+		
+		for (int i=0; i < numDataPts; ++i) {
+			// Forecast vs real process
+			data[i][1] = mData.get(i)[0];
+			data[i][0] = mData.get(i)[2];
+			dataStdDev[i][1] = mData.get(i)[1]; 
+			dataStdDev[i][0] = 0; 
+			
+			// Forecast vs real process shifted
+			dataLag[i][1] = mData.get(i)[0];
+			dataLag[i+lag][0] = mData.get(i)[2];
+			dataLagStdDev[i][1] = mData.get(i)[1]; 
+			dataLagStdDev[i+lag][0] = 0; 
+		}
+		// Fill in end of prediction graph / beginning of real graph
+		for (int i=0; i < lag; i++) {
+			// Forecast vs real process
+			data[numDataPts+i][1] = data[numDataPts-1][1];
+			data[numDataPts+i][0] = data[numDataPts-1][0];
+			dataStdDev[numDataPts+i][1] = dataStdDev[numDataPts-1][1]; 
+			dataStdDev[numDataPts+i][0] = dataStdDev[numDataPts-1][0]; 
+			
+			// Forecast vs real process shifted
+			dataLag[numDataPts+i][1] = dataLag[numDataPts-1][1];
+			dataLag[i][0] = 0;
+			dataLagStdDev[numDataPts+i][1] = dataLagStdDev[numDataPts-1][1]; 
+			dataLagStdDev[i][0] = 0; 
+		}
+		XYDataset dataset = AnalysisUtils.getDatasetFromArray(data, dataStdDev, stepSize, names);
+		PCTMCChartUtilities.drawDeviationChart(dataset, "time", "#arrivals", "Forecast Vs Oracle - "+mCurDate, analysis);
+		XYDataset datasetLag = AnalysisUtils.getDatasetFromArray(dataLag, dataLagStdDev, stepSize, namesLag);
+		PCTMCChartUtilities.drawDeviationChart(datasetLag, "time", "#arrivals", "Forecast Vs Latest Measurement - "+mCurDate, analysis);
+	}
 }
