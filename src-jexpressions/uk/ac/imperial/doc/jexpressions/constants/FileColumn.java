e81f51c717493b71250ac1bb8069f0631c430084
Anton Stefanek <as1005@doc.ic.ac.uk>
2012-11-21 12:20:11 +0000

    Added missing file.

---
+++ src-jexpressions/uk/ac/imperial/doc/jexpressions/constants/FileColumn.java
@@ -0,0 +1,23 @@
+package uk.ac.imperial.doc.jexpressions.constants;
+
+public class FileColumn {
+	private String file;
+	private int column;
+	
+	public FileColumn(String file, int column) {
+		super();
+		this.file = file;
+		this.column = column;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+	
+	
+	
+}
