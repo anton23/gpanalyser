8177ef1b90b9d9216357a4367fa7398064bed2ae
tonkos <tonkos@gmail.com>
2013-02-13 21:38:01 +0000

    Replaced RungeKutta code with a wrapper of the respective apache-commons method.

--- src-pctmc/uk/ac/imperial/doc/pctmc/odeanalysis/utils/RungeKutta.java
+++ src-pctmc/uk/ac/imperial/doc/pctmc/odeanalysis/utils/RungeKutta.java
@@ -1,10 +1,66 @@
 package uk.ac.imperial.doc.pctmc.odeanalysis.utils;
 
+import java.util.Arrays;
+
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.exception.MaxCountExceededException;
+import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
+import org.apache.commons.math3.ode.FirstOrderIntegrator;
+import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
+import org.apache.commons.math3.ode.sampling.FixedStepHandler;
+import org.apache.commons.math3.ode.sampling.StepNormalizer;
+
 
 public class RungeKutta {
-	// see wikipedia
+	
+	
+	public static double[][] rungeKutta(final SystemOfODEs f, double initial[],
+			double stopTime, final double stepSize, int density) {
+		final int n = initial.length;
+
+		double[] init = Arrays.copyOf(initial, n);		
+		FirstOrderIntegrator integrator = new ClassicalRungeKuttaIntegrator(stepSize / density);
+		final double ret[][] = new double[(int) Math.ceil(stopTime / stepSize)][];
+		
+		FixedStepHandler fixedStepHandler = new FixedStepHandler() {
+			
+			int step;
+			
+		    public void init(double t0, double[] y0, double t) {step = 0;}
+
+			public void handleStep(double t, double[] y, double[] yDot,
+					boolean isLast) {
+				ret[step++] = Arrays.copyOf(y, n);
+				
+			}
+		};
+		
+		integrator.addStepHandler(new StepNormalizer(stepSize, fixedStepHandler));
+		FirstOrderDifferentialEquations ode = new FirstOrderDifferentialEquations() {
+			
+			@Override
+			public int getDimension() {
+				return n;
+			}
+			
+			@Override
+			public void computeDerivatives(double t, double[] y, double[] yDot)
+					throws MaxCountExceededException, DimensionMismatchException {
+				double[] yDotF = f.derivn(t, y);
+				for (int i = 0; i < n; i ++) {
+					yDot[i] = yDotF[i];
+				}				
+			}
+		};
+		
+		integrator.integrate(ode, 0.0, init, stopTime, init);
+		return ret;
+		
+	}
+	
+/*	// see wikipedia
 	public static double[][] rungeKutta(SystemOfODEs f, double initial[],
-			double stopTime, double stepSize, int density) {
+			double stopTime, double stepSize, int density) {		
 		double h = stepSize / density;
 		double x = 0;
 		int n = initial.length;
@@ -51,5 +107,5 @@ public class RungeKutta {
 			x = x + h;
 		}
 		return ret;
-	}
+	}*/
 }
