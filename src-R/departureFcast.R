f76537287ebbf0fea47f8c187b0221ccd503816d
machr666 <machr666@localhost>
2014-03-11 17:54:44 +0000

    R integration now works

--- src-R/departureFcast.R
+++ src-R/departureFcast.R
@@ -5,22 +5,23 @@ trainDepartureFcastModels <- function (
   depFcastMode,
   fcastFreq = 5,
   trainDepTSFiles = NULL,
-  trainMuDepTSFiles = NULL
+  trainDepToDestTSFiles = NULL
 ) {
   switch(depFcastMode,
     naive = trainNaiveDepFcast(fcastFreq),
-    arima = trainArimaDepFcast(fcastFreq, trainDepTSFiles, trainMuDepTSFiles),
-    oracle = trainOracleDepFcast(fcastFreq, trainDepTSFiles, trainMuDepTSFiles)
+    arima = trainArimaDepFcast(fcastFreq, trainDepTSFiles, trainDepToDestTSFiles),
+    oracle = trainORACLEDepFcast(fcastFreq)
   )
 }
 
 trainNaiveDepFcast <- function(fcastFreq) {
   list(name = "NaiveDepartureForecast",
-    genTS = function(depTS, muTS, start, warmup, fcastLen) {
-      deps <- muTS[start : (start + warmup - 1)] * 
-        depTS[start : (start + warmup - 1)]
-      futureDepPerFreq <- mean(tail(deps, fcastFreq))
-      deps <- c(deps, rep(futureDepPerFreq, fcastLen))
+    genTS = function(depTS, depToDestTS, start, warmup, fcastLen) {
+      # Check that the forecast length is a multiple of the forecast frequency
+      assert_that(fcastLen %% fcastFreq == 0)
+      deps <- depToDestTS[start : (start + warmup - 1)]
+      futureDepPerFreq <- tail(deps, fcastFreq)
+      c(deps, rep(futureDepPerFreq, (fcastLen / fcastFreq)))
     }
   )
 }
@@ -28,41 +29,40 @@ trainNaiveDepFcast <- function(fcastFreq) {
 trainARIMADepFcast <- function (
   fcastFreq,
   trainDepTSFiles,
-  trainMuDepTSFiles
+  trainDepToDestTSFiles
 ) {
   
 }
 
-trainORACLEDepFcast <- function (
-  fcastFreq,
-  trainDepTSFiles,
-  trainMuDepTSFiles
-) {
-
+trainORACLEDepFcast <- function (fcastFreq) {
+  list(name = "OracleDepartureForecast",
+    genTS = function(depTS, depToDestTS, start, warmup, fcastLen) {
+      depToDestTS[start : (start + warmup + fcastLen - 1)]
+    }
+  )
 }
 
 genDepartureTS <- function (
   depModel,
   depTSFile,
-  muTSFile,
+  depToDestTSFile,
   start,
   warmup,
   fcastLen
 ) {
   depTS <- loadFile(depTSFile)
-  muTS <- loadFile(muTSFile)
-  assert_that(all(dim(depTS) == dim(muTS)))
-
+  depToDestTS <- loadFile(depToDestTSFile)
+  assert_that(all(dim(depTS) == dim(depToDestTS)))
+  
   # Return departure time series (known + forecast) when possible
   if (start + warmup + fcastLen >= dim(depTS)[2]) {
     return(NULL)
   }
   deps = matrix(nrow = dim(depTS)[1], ncol = warmup + fcastLen)
   for (i in 1 : dim(depTS)[1]) {
-    cat(depTS[i, which(muTS[i,] != 0)], '\n')
-    # Each mu must have a matching departure
-    assert_that(length(which(depTS[i, which(muTS[i,] != 0)] == 0)) == 0)
-    deps[i,] <- depModel$genTS(depTS[i,], muTS[i,], start, warmup, fcastLen)
+    # Each observation in depToDestTS must be smaller than matching departure
+    assert_that(length(which((depTS[i,] - depToDestTS[i,]) < 0)) == 0)
+    deps[i,] <- depModel$genTS(depTS[i,], depToDestTS[i,], start, warmup, fcastLen)
   }
   deps
 }
